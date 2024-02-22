7c02372ce5893a3c21d5ed1b7f69371e3bfa73b6
spidermatsu <62445737+spidermatsu@users.noreply.github.com>
2024-02-22 13:57:44 +0000

    Create week-1.py

---
+++ week-1.py
@@ -0,0 +1,8 @@
+#Q: What other number systems do we use?
+
+#A: Binary, hexadecimal, roman numeral and other non latin representations
+
+#Q: Why do computers use binary? Why not decimal?
+
+#A: On an electronic level, circuit parts can only have 2 states pass through them - high voltage and low voltage
+#this in turn represents 1s and 0s
